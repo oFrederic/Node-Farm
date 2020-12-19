3cc95286b7900b87fd563d3f0818cb892324c980
Wojcikowski Frederic <wojcikowskif@gmail.com>
2020-12-19 20:52:56 +0900

    Creating a simple web server

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
