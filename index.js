557155b30f22692a9a76327d1d7aa14a00cf9331
Wojcikowski Frederic <wojcikowskif@gmail.com>
2020-12-19 21:01:05 +0900

    Routing

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const pathName = req.url;
+
+  switch (pathName) {
+    case '/':
+    case '/overview':
+      res.end('OVERVIEW');
+      break;
+    case '/product':
+      res.end('PRODUCT');
+      break;
+    default:
+      res.writeHead(404, { 'Content-type': 'text/html' });
+      res.end('<h1>Page not found!<h1>');
+      break;
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
