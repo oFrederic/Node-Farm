ffcec043503837b4ebdaa469acb39efbd98a45f2
Wojcikowski Frederic <wojcikowskif@gmail.com>
2020-12-20 00:21:00 +0900

    Using our own modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(
   './templates/template-overview.html',
@@ -15,20 +16,6 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
-
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
@@ -39,19 +26,25 @@ const server = http.createServer((req, res) => {
       const cardsHtml = dataObj
         .map(item => replaceTemplate(tempCard, item))
         .join('');
-      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-      res.end(output);
+      const cardsHtmlOutput = tempOverview.replace(
+        '{%PRODUCT_CARDS%}',
+        cardsHtml
+      );
+      res.end(cardsHtmlOutput);
       break;
+
     case '/product':
       res.writeHead(200, { 'Content-type': 'text/html' });
       const product = dataObj[query.id];
       const productOutput = replaceTemplate(tempProduct, product);
       res.end(productOutput);
       break;
+
     case '/api':
       res.writeHead(200, { 'Content-type': 'application/json' });
       res.end(data);
       break;
+
     default:
       res.writeHead(404, { 'Content-type': 'text/html' });
       res.end('<h1>Page not found!<h1>');
---
+++ modules/replaceTemplate.js
@@ -0,0 +1,13 @@
+module.exports = (template, product) => {
+  let output = template.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
