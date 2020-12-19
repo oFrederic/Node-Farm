efe40848db27524a75d123cc6010d9911c028ec7
Wojcikowski Frederic <wojcikowskif@gmail.com>
2020-12-19 23:20:13 +0900

    HTML templating: filling the templates

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const fs = require('fs');
 const http = require('http');
 
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   './templates/template-overview.html',
   'utf-8'
@@ -21,7 +35,11 @@ const server = http.createServer((req, res) => {
     case '/':
     case '/overview':
       res.writeHead(200, { 'Content-type': 'text/html' });
-      res.end(tempOverview);
+      const cardsHtml = dataObj
+        .map(item => replaceTemplate(tempCard, item))
+        .join('');
+      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+      res.end(output);
       break;
     case '/product':
       res.writeHead(200, { 'Content-type': 'text/html' });
